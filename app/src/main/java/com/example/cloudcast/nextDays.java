1ea274bdc5a545e43c974e5157a25154a0816c58
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-07-15 22:01:08 +0600

    next 5 days forecast implementation

--- app/src/main/java/com/example/cloudcast/nextDays.java
+++ app/src/main/java/com/example/cloudcast/nextDays.java
@@ -2,13 +2,28 @@ package com.example.cloudcast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class nextDays extends AppCompatActivity {
 
+    TextView lon, lat;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_days);
+
+        lon = findViewById(R.id.lon);
+        lat = findViewById(R.id.lat);
+        // getting longitude & latitude
+        Bundle bundle = getIntent().getExtras();
+        double lonV = bundle.getDouble("lon", 0);
+        double latV = bundle.getDouble("lat", 0);
+
+        lon.setText(String.format("%.2f", lonV));
+        lat.setText(String.format("%.2f", latV));
     }
 }
